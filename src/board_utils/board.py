d54f1fa90be0552d609c91ba7759d3f925ab2f7e
Daniel Goman <gomanGGGoman@gmail.com>
2022-12-24 19:49:30 +0200

    Added board cropping by sequence of detected edges

--- src/board_utils/board.py
+++ src/board_utils/board.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import itertools
 
 from matplotlib import pyplot as plt
 from src.input_utils.image_capture import ImageCapture
@@ -39,7 +40,8 @@ class Board:
 
         return frame, key_points, descriptors
 
-    def crop_by_sum(self, edges):
+    @staticmethod
+    def crop_by_sum(edges):
         height, width = edges.shape[:2]
         portion = 0.6
         x_sum = np.sum(edges, axis=1)
@@ -57,6 +59,48 @@ class Board:
         print(f'end: ({x_end}, {y_end})\n')
 
         plt.imsave('sum_crop.png', edges[x_start: x_end, y_start: y_end], cmap='gray')
+
+    @staticmethod
+    def crop_by_seq(edges, portion=0.1):
+        height, width = edges.shape
+        row_seq = Board.get_max_seq_lens_per_row(edges)
+        col_seq = Board.get_max_seq_lens_per_row(edges.T)
+
+        x_start = np.argmax(row_seq > portion * width)
+        y_start = np.argmax(col_seq > portion * height)
+
+        x_end = height - np.argmax(row_seq[::-1] > portion * width)
+        y_end = width - np.argmax(col_seq[::-1] > portion * height)
+
+        print('Crop by seq:')
+
+        print(f'start: ({x_start}, {y_start})')
+        print(f'end: ({x_end}, {y_end})\n')
+
+        plt.imsave('seq_crop.png', edges[x_start: x_end, y_start: y_end], cmap='gray')
+
+    @staticmethod
+    def get_max_seq_lens_per_row(frame):
+        height, width = frame.shape[:2]
+        padding = np.zeros((height, 1))
+        stacked = np.hstack([padding, frame == 1, padding])
+        diff = np.diff(stacked)
+        rows_number, seq_idx = np.where(diff)
+
+        row_unique_seqs_number = [len(list(values)) for key, values in itertools.groupby(rows_number)]
+        row_seq_idx_split = np.cumsum(row_unique_seqs_number)
+
+        seq_idx_list_per_row = np.split(seq_idx, row_seq_idx_split)[:-1]
+        seq_idx_list_per_row = [row_seq_indices.reshape(-1, 2) for row_seq_indices in seq_idx_list_per_row]
+        seq_lens_per_row = [np.diff(row_seq_indices) for row_seq_indices in seq_idx_list_per_row]
+
+        max_seq_len_per_row = [np.max(row_seq_lens) for row_seq_lens in seq_lens_per_row]
+
+        max_seq_len_per_row_no_holes = np.zeros(height)
+        existent_rows = np.unique(rows_number)
+        np.put(max_seq_len_per_row_no_holes, existent_rows, max_seq_len_per_row)
+
+        return max_seq_len_per_row_no_holes
 
 
 if __name__ == "__main__":
