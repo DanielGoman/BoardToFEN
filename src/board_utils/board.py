e523c6bf4366d0996fb3cbb29dcaa0c0a55da2bf
Daniel Goman <gomanGGGoman@gmail.com>
2022-12-24 20:16:33 +0200

    Added some comments to get_max_seq_lens_per_row

--- src/board_utils/board.py
+++ src/board_utils/board.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import cv2
 import numpy as np
 import itertools
@@ -61,7 +63,7 @@ class Board:
         plt.imsave('sum_crop.png', edges[x_start: x_end, y_start: y_end], cmap='gray')
 
     @staticmethod
-    def crop_by_seq(edges, portion=0.1):
+    def crop_by_seq(edges: np.ndarray, portion=0.1) -> (np.ndarray, np.ndarray, Tuple[int, int], Tuple[int, int]):
         height, width = edges.shape
         row_seq = Board.get_max_seq_lens_per_row(edges)
         col_seq = Board.get_max_seq_lens_per_row(edges.T)
@@ -79,23 +81,47 @@ class Board:
 
         plt.imsave('seq_crop.png', edges[x_start: x_end, y_start: y_end], cmap='gray')
 
+        return row_seq, col_seq, (x_start, x_end), (y_start, y_end)
+
     @staticmethod
-    def get_max_seq_lens_per_row(frame):
+    def get_max_seq_lens_per_row(frame: np.ndarray) -> np.ndarray:
+        """Retrieves the length of the longest edge in all rows of the given matrix
+
+        Args:
+            frame: the matrix in which we search for the longest edge in each row
+
+        Returns:
+            max_seq_len_per_row_no_holes: index i contains the length of the longest edge in row i
+
+        """
         height, width = frame.shape[:2]
+
+        # padding each row to also capture sequences that start from the first index
         padding = np.zeros((height, 1))
         stacked = np.hstack([padding, frame == 1, padding])
+
+        # subtracting consecutive indexes
         diff = np.diff(stacked)
+
+        # Finds the start and end of all the sequences
         rows_number, seq_idx = np.where(diff)
 
+        # number of the sequences found per row - if found 2 sequences in some row, then the lengthfor that row
+        # would be 4:   start_idx_seq1, end_idx_seq1, start_idx_seq2, end_idx_seq2
         row_unique_seqs_number = [len(list(values)) for key, values in itertools.groupby(rows_number)]
+        # start and end of the sequences list of each row
         row_seq_idx_split = np.cumsum(row_unique_seqs_number)
 
+        # separate row-wise the sets of sequence indexes and calculate the length of each sequence
         seq_idx_list_per_row = np.split(seq_idx, row_seq_idx_split)[:-1]
         seq_idx_list_per_row = [row_seq_indices.reshape(-1, 2) for row_seq_indices in seq_idx_list_per_row]
         seq_lens_per_row = [np.diff(row_seq_indices) for row_seq_indices in seq_idx_list_per_row]
 
+        # calculate the maximal length of a sequence in each row
         max_seq_len_per_row = [np.max(row_seq_lens) for row_seq_lens in seq_lens_per_row]
 
+        # single list with max length of the sequence in each row.
+        # rows with no sequence at all get a 0 value
         max_seq_len_per_row_no_holes = np.zeros(height)
         existent_rows = np.unique(rows_number)
         np.put(max_seq_len_per_row_no_holes, existent_rows, max_seq_len_per_row)
@@ -104,10 +130,16 @@ class Board:
 
 
 if __name__ == "__main__":
-    board_template_path = r'../../data/board_template.png'
-    board = Board(board_template_path=board_template_path)
+    path = "../../data/real_board.png"
+    image = cv2.imread(path)
 
-    cap = ImageCapture()
-    img = cap.capture()
-    board.get_descriptors(frame=img)
+    print('Image shape:', image.shape[:2])
+    print()
+
+    # edge detector
+    edges = cv2.Canny(image, 50, 250)
+    edges[edges < 50] = 0
+    edges[edges >= 50] = 1
+
+    row_seq, col_seq, x_crop_idx, y_crop_idx = Board.crop_by_seq(edges)
 
