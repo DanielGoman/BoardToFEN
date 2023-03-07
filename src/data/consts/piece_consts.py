fc42eb0e26ff5aa5b1b489abca5d67f7c8c4a219
Daniel Goman <gomanGGGoman@gmail.com>
2023-03-07 22:38:17 +0200

    Finished train loop, still requires testing

--- src/data/consts/piece_consts.py
+++ src/data/consts/piece_consts.py
@@ -9,6 +9,8 @@ PIECE_TYPE = {'P': 0,
               'K': 5,
               NON_PIECE: 6}
 
+REVERSED_PIECE_TYPE = {val: key for key, val in PIECE_TYPE.items()}
+
 PIECE_COLOR = {'W': 0,
                'B': 1,
                NON_PIECE: 2}
--- src/model/train.py
+++ src/model/train.py
@@ -19,6 +19,8 @@ def train():
     lr = train_configs['lr']
     momentum = train_configs['momentum']
     num_epochs = train_configs['num_epochs']
+    print_interval = train_configs['print_interval']
+    model_path = train_configs['model_path']
 
     dataset = PiecesDataset(images_dir_path=data_configs['IMAGE_DIR_PATH'],
                             labels_path=data_configs['LABELS_JSON_PATH'])
@@ -38,15 +40,38 @@ def train():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, momentum=momentum)
 
+    print('Starting training')
     model.train()
     for epoch in range(num_epochs):
-        for i, data in enumerate(train_loader):
-            image, piece_type, piece_color, is_piece = data
+        epoch_loss = 0.0
+        interval_loss = 0.0
+        for iter_num, data in enumerate(train_loader):
+            image, type_label, color_label, is_piece = data
 
             optimizer.zero_grad()
 
             type_pred, color_pred = model(image)
 
+            loss = criterion(type_pred, type_label)
+            if is_piece:
+                loss += criterion(color_pred, color_label)
+
+            loss.backward()
+            optimizer.step()
+
+            interval_loss += loss.item()
+            epoch_loss += loss.item()
+
+            if iter_num % print_interval == 0:
+                print(f'epoch: {epoch}, iteration: {iter_num}, loss: {interval_loss / print_interval}:.3f')
+                interval_loss = 0.0
+
+        print(f'epoch {epoch} loss: {epoch_loss / len(train_size)}:.3f\n')
+
+    print('Finished training\n')
+    print('Saving model')
+    model_scripted = torch.jit.script(model)
+    model_scripted.save(model_path)
 
 
 if __name__ == "__main__":
