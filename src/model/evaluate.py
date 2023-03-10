afc081cd37e535f34bd3e593f5b5e810e16d4dde
Daniel Goman <gomanGGGoman@gmail.com>
2023-03-10 22:16:09 +0200

    Added accuracy calculating on a subset of train and val every epoch

--- src/model/evaluate.py
+++ src/model/evaluate.py
@@ -3,7 +3,7 @@ import torch
 from src.data.consts.piece_consts import REVERSED_PIECE_TYPE, REVERSED_PIECE_COLOR, NON_PIECE, PIECE_COLOR
 
 
-def eval_model(model, loader: torch.utils.data.DataLoader, state: str):
+def eval_model(model, loader: torch.utils.data.DataLoader, state: str, verbose: bool = True, eval_size: float = 1.0):
     """Evaluates the per-class type and color accuracy, as well as a balanced accuracy for type and class
 
     Args:
@@ -12,6 +12,8 @@ def eval_model(model, loader: torch.utils.data.DataLoader, state: str):
         state: the type of dataset the model is run on (train or test)
 
     """
+    subset_size = int(len(loader) * eval_size)
+    model.eval()
     with torch.no_grad():
         num_piece_classes = len(REVERSED_PIECE_TYPE)
         num_piece_colors = len(REVERSED_PIECE_COLOR)
@@ -21,7 +23,7 @@ def eval_model(model, loader: torch.utils.data.DataLoader, state: str):
         color_correct_hits = torch.zeros(num_piece_colors)
         color_counts = torch.zeros(num_piece_colors)
 
-        for image, type_label, color_label, is_piece in loader:
+        for i, (image, type_label, color_label, is_piece) in enumerate(loader):
             type_pred_probs, color_pred_probs = model(image)
             type_pred = torch.argmax(type_pred_probs, axis=1)
             color_pred = torch.argmax(color_pred_probs, axis=1)
@@ -44,18 +46,21 @@ def eval_model(model, loader: torch.utils.data.DataLoader, state: str):
         type_accuracy = type_correct_hits / type_counts
         color_accuracy = color_correct_hits / color_counts
 
-        print(f'\nResults over the {state} set\n')
-        for piece_type, piece_name in REVERSED_PIECE_TYPE.items():
-            print(f'Accuracy for {piece_name}: {type_accuracy[piece_type]:.3f}')
-
-        for piece_color, color_name in REVERSED_PIECE_COLOR.items():
-            print(f'Accuracy for {color_name}: {color_accuracy[piece_color]:.3f}')
-
         type_rates = type_counts / type_counts.sum()
         color_rates = color_counts / color_counts.sum()
         balanced_type_accuracy = type_accuracy @ type_rates
         balanced_color_accuracy = color_accuracy @ color_rates
 
-        print()
-        print(f'Balanced type accuracy: {balanced_type_accuracy.item():.3f}')
-        print(f'Balanced color accuracy: {balanced_color_accuracy.item():.3f}')
+        if verbose:
+            print(f'\nResults over the {state} set\n')
+            for piece_type, piece_name in REVERSED_PIECE_TYPE.items():
+                print(f'Accuracy for {piece_name}: {type_accuracy[piece_type]:.3f}')
+
+            for piece_color, color_name in REVERSED_PIECE_COLOR.items():
+                print(f'Accuracy for {color_name}: {color_accuracy[piece_color]:.3f}')
+
+            print()
+            print(f'Balanced type accuracy: {balanced_type_accuracy.item():.3f}')
+            print(f'Balanced color accuracy: {balanced_color_accuracy.item():.3f}')
+
+        return balanced_type_accuracy, balanced_color_accuracy
