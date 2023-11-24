bc219c5a9e7d16a0d7d689f9d22a44384bced540
Daniel Goman <gomanGGGoman@gmail.com>
2023-11-24 22:18:26 +0200

    Cleaned up and documented `input_utils` dir

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from src.board_utils.board import Board
 
 
 def main():
-    board_template_path = r'src/data/empty_board_template.png'
-    board = Board(board_template_path=board_template_path)
-
-    controller = KeyboardController(board=board)
+    controller = KeyboardController()
     controller.start_listener()
-
 
 
 if __name__ == "__main__":
