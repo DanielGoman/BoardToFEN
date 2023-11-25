18ad2503ca7253933a02ae7f1432a6501e7c7c3a
Daniel Goman <gomanGGGoman@gmail.com>
2023-11-25 20:36:21 +0200

    Added docs to fen_utils.py and improved variable names

--- src/fen_converter/fen_utils.py
+++ src/fen_converter/fen_utils.py
@@ -8,6 +8,15 @@ from src.data.consts.squares_consts import BOARD_SIDE_SIZE
 
 
 def convert_row_to_fen(row: np.ndarray) -> str:
+    """Converts a board row to a fen representation of that row
+
+    Args:
+        row: row of integers representing pieces in the model's predictions format
+
+    Returns:
+        fen: a fen representation of a row of a chess board
+
+    """
     start_indices, sequence_lengths = find_sequence_indices(row, value_to_count=LABELS['X'])
 
     idx = 0
@@ -28,13 +37,29 @@ def convert_row_to_fen(row: np.ndarray) -> str:
 
 
 def find_sequence_indices(arr: np.ndarray, value_to_count: int) -> Tuple[List[int], List[int]]:
+    """Finds the start index and length of every sequence of the `value_to_count` value
+
+    Args:
+        arr: 1d array
+        value_to_count: the value of which we're interested in finding its sequences in `arr`
+
+    Returns:
+        start_indices: list of the first index of each sequence
+        sequence_lengths: list of lengths of each sequence found
+
+    """
     start_indices = []
     sequence_lengths = []
 
-    for k, g in groupby(enumerate(arr == value_to_count), key=lambda x: x[1]):
-        if k:
-            start_index, _ = next(g)
-            length = len(list(g)) + 1  # Include the starting element
+    # group-by matching sequences and non-matching sequences
+    for is_desired_sequence, sequence_iterator in groupby(enumerate(arr == value_to_count), key=lambda x: x[1]):
+        # skips non-matching sequences
+        if is_desired_sequence:
+            # (enumerate-index, value inside the sequence)
+            start_index, _ = next(sequence_iterator)
+            # include the starting element we just dropped using `next`
+            length = len(list(sequence_iterator)) + 1
+
             start_indices.append(start_index)
             sequence_lengths.append(length)
 
@@ -42,8 +67,8 @@ def find_sequence_indices(arr: np.ndarray, value_to_count: int) -> Tuple[List[in
 
 
 if __name__ == "__main__":
-    arr = np.array([11, 12, 0, 12, 12, 0, 12, 0])
+    _arr = np.array([11, 12, 0, 12, 12, 0, 12, 0])
 
-    print(convert_row_to_fen(arr))
+    print(convert_row_to_fen(_arr))
 
 
